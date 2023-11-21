ac5f6878f4f7d7566dfbdfffa59aa124fd209f37
mayankmehar9128 <150352254+mayankmehar9128@users.noreply.github.com>
2023-11-21 15:49:14 +0530

    Add files via upload

---
+++ caculator.js
@@ -0,0 +1,62 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+let operators = document.querySelectorAll('.operator')
+// console.log(buttons);
+
+operators.forEach(function(ope){
+    console.log(ope);
+    ope.addEventListener('click', function(singh){
+
+        if (string === "" && singh.target.innerHTML !== '-') { // Add other operators as needed
+            alert("Please enter a number first!");
+        }
+        
+        else {
+            string = string + singh.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+})
+
+buttons.forEach(function (value) {
+    // console.log(value);
+    console.log(value);
+    value.addEventListener('click', function(e){
+        console.log(e.target);
+
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        } 
+        // else if (singh.target.innerHTML == '/') {
+        //      opera_singh = ""
+        //     document.querySelector('input').value = opera_singh;
+        // }
+        // else if (singh.target.innerHTML == '*') {
+        //      opera_singh = ""
+        //     document.querySelector('input').value = opera_singh;
+        // }
+        // else if (singh.target.innerHTML == '+') {
+        //      opera_singh = ""
+        //     document.querySelector('input').value = opera_singh;
+        // }
+        // else if (singh.target.innerHTML == 'M+') {
+        //      opera_singh = ""
+        //     document.querySelector('input').value = opera_singh;
+        // }
+
+        else if (e.target.innerHTML == 'C') {
+            string = "";
+            document.querySelector('input').value = string; 
+        }
+
+        else {
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+           
+    })
+})
+
+
+
